dbafb63d65c8c90d4138704e01be7246de6f2a85
Rishabh <rishabhgpt04@gmail.com>
2022-01-06 02:12:21 +0530

    url fixed

--- src/App.js
+++ src/App.js
@@ -1,43 +1,37 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-import "./App.css";
-import Navbar from "./components/Navbar";
+import './App.css';
+import Navbar from './components/Navbar';
 import NewsContent from "./components/NewsContent/NewsContent";
 import apikey from "./data/config";
 import Footer from "./Footer/Footer";
 function App() {
-  const [category, setcategory] = useState("general");
+  const [category, setcategory] = useState('general')
   const [NewsResults, setNewsResults] = useState();
   const [NewsArray, setNewsArray] = useState([]);
-  const [Loadmore, setLoadmore] = useState(20);
-  const NewsApi = async () => {
-    try {
+  const [Loadmore, setLoadmore] = useState(20)
+  const NewsApi = async () =>{
+    try{
       const proxyUrl = "https://cors-anywhere.herokuapp.com/";
-      const news = await axios.get(
-        `${proxyUrl}https://newsapi.org/v2/top-headlines?country=in&apiKey=${apikey}&category=${category}&pageSize=${Loadmore}`
-      );
-      setNewsArray(news.data.articles);
-      setNewsResults(news.data.totalResults);
-    } catch (error) {
+      const news= await axios.get(`https://newsapi.org/v2/top-headlines?country=in&apiKey=${apikey}&category=${category}&pageSize=${Loadmore}`);
+    setNewsArray(news.data.articles);
+    setNewsResults(news.data.totalResults);
+    }
+    catch(error){
       console.log(error);
     }
-  };
-  useEffect(() => {
+  }
+  useEffect(()=>{
     NewsApi();
-    // eslint-disable-next-line
-  }, [NewsResults, category, Loadmore]);
+  },[NewsResults, category,Loadmore])
 
   return (
     <div className="App">
-      <Navbar setcategory={setcategory} />
-      <NewsContent
-        Loadmore={Loadmore}
-        setLoadmore={setLoadmore}
-        NewsArray={NewsArray}
-        NewsResults={NewsResults}
-      />
+      <Navbar setcategory={setcategory}/>
+      <NewsContent Loadmore={Loadmore} setLoadmore={setLoadmore} NewsArray= {NewsArray} NewsResults={NewsResults}/> 
       <Footer />
     </div>
+  
   );
 }
 
